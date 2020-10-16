601b35abdfdac34a5124bdbbc8c04cd50355c410
jjack27 <yizhouzhao27@gmail.com>
2020-10-15 20:07:27 -0600

    add default view

--- hubor/hubor/urls.py
+++ hubor/hubor/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from accounts.views import MyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('/', MyView.as_view())
 ]
