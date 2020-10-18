6809465f72d1b3018ca23e43515935332d991528
JJack27 <yizhouzhao27@gmail.com>
2020-10-17 22:05:17 -0600

    add views_emergency.py

---
+++ hubor/api/views_emergency.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+import json
+import uuid
+from django.contrib.auth import login, logout, authenticate
+from django.core.mail import send_mail
+import datetime
+from emergency.models import *
+from emergency.serializers import *
