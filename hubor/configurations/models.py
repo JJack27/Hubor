6e55d1a092c7fd5308a7ed023c015b2490e8e3ce
jjack27 <yizhouzhao27@gmail.com>
2020-10-20 12:33:25 -0600

    add model

--- hubor/configurations/models.py
+++ hubor/configurations/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Configuration(models.Model):
+    NAME_CHOICES = (
+        ("TEM", "tem"),
+        ("ACX", "acx"),
+        ("ACZ", "acz"),
+        ("BAT", "bat"),
+        ("RED", "red"),
+        ("IR", "ir")
+    )
+
+    COMPARE_CHOICES = (
+        (0, "greater"),
+        (1, "less"),
+        (2, "within")
+    )
+    id = models.AutoField(primary_key=True)
+    name = models.ChoiceField(null=False, choices=NAME_CHOICES, default="TEM")
+    
+    # Compare methods: 0-greater, 1-less, 2-within
+    compare = models.ChoiceField(null=False, choices=COMPARE_CHOICES, default="0")
+    
+    # Range for comparison, default is the min/max of the integer
+    range_min = models.FloatField(null = False, default=0x80000000)
+    range_max = models.FloatField(null = False, default=0x7fffffff)
+
+    # In the unit of seconds
+    duration = models.IntegerField(null = False, default = 0)
