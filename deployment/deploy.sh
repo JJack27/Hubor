d1dc9c00d1ce01a6e4083abadd6c6be7e0fb42a7
jjack27 <yizhouzhao27@gmail.com>
2021-01-01 13:55:40 -0700

    add deploy.sh

---
+++ deployment/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Deploying the web application"
+
+# check arguments
+if ["$#" == 0]; then 
+    echo "Invalid number of arguments."
+    echo "Please follow the following format:"
+    echo "\tdeploy.sh <absolute-dir-of-the-project> <name-of-the-project>"
+else
+    echo "Directory of the project: $0"
+    echo "Name of the project: $1"
+fi
+
+# create virtual env of python
+sudo apt update
+sudo apt install python3-pip
+
+# install required libraries
+pip3 install virtualenv
+virtualenv -p python3 "$1"
+# migrate the database
+# initialize the project
+# build the frontend project
+# collect static files
+# write <project-name>.socket
+# write <project-name>.service
+# write <project-name> nginx conf
+# start the project
