73edc9a00da08a13b519f1e6b56d727d406c8719
JJack27 <yizhouzhao27@gmail.com>
2021-04-01 15:12:04 +0800

    added chart

---
+++ hubor/frontend/src/components/ChartsPlots/VSMultiViewChart.vue
@@ -0,0 +1,108 @@
+<template>
+    <div id="container">
+
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { MultiView } from '@antv/g2plot';
+
+export default defineComponent({
+    name:"VSMultiViewChart",
+
+    
+    setup() {
+        console.log("chart");
+        const chart = fetch('https://gw.alipayobjects.com/os/antfincdn/qE48lpzwRc/range-area-data.json')
+            .then((data) => data.json())
+            .then((data) => {
+                console.log(data);
+                const { area: data1, line: data2 } = data;
+                const rangeAreaPlot = new MultiView('container', {
+                    appendPadding: 8,
+                    syncViewPadding: true,
+                    tooltip: { shared: true, showMarkers: false, showCrosshairs: true, offsetY: -50 },
+                    views: [
+                        {
+                        data: data1,
+                        axes: {},
+                        meta: {
+                            time: {
+                            type: 'time',
+                            mask: 'MM-DD',
+                            nice: true,
+                            tickInterval: 24 * 3600 * 1000 * 2,
+                            range: [0, 1],
+                            },
+                            temperature: {
+                            nice: true,
+                            sync: true,
+                            alias: '温度范围',
+                            },
+                        },
+                        // view1: prepare a area plot, mapping position to `time*temperature`
+                        geometries: [
+                            {
+                            type: 'area',
+                            xField: 'time',
+                            yField: 'temperature',
+                            mapping: {},
+                            },
+                        ],
+                        },
+                        {
+                        data: data2,
+                        axes: false,
+                        meta: {
+                            time: {
+                            type: 'time',
+                            mask: 'MM-DD',
+                            nice: true,
+                            tickInterval: 24 * 3600 * 1000 * 2,
+                            range: [0, 1],
+                            },
+                            temperature: {
+                            sync: 'temperature',
+                            alias: '温度',
+                            },
+                        },
+                        // view2: prepare a line plot and point plot, mapping position to `time*temperature` (share data)
+                        geometries: [
+                            {
+                            type: 'line',
+                            xField: 'time',
+                            yField: 'temperature',
+                            mapping: {},
+                            },
+                            {
+                            type: 'point',
+                            xField: 'time',
+                            yField: 'temperature',
+                            mapping: {
+                                shape: 'circle',
+                                style: {
+                                fillOpacity: 1,
+                                },
+                            },
+                            },
+                        ],
+                        },
+                    ],
+                });
+
+            // Step 3: 渲染图表
+            rangeAreaPlot.render();
+        });
+
+        return {chart};
+        
+    },
+
+
+})
+</script>
+
+<style>
+
+</style>
--- hubor/frontend/src/views/Test.vue
+++ hubor/frontend/src/views/Test.vue
@@ -1,54 +1,5 @@
 <template>
-    <div id="card-body" style="height:100vh; width:25%; background:#444444">
-        <a-row type="flex" justify="bottom" :gutter="[16,16]">
-            <a-col :span="8">
-                <VSEntryVertical
-                    icon="rr_icon.png"
-                    :value="60"
-                    unit="rpm"
-                    title="RR"
-                />
-            </a-col>
-
-            <a-col :span="16">
-                <VSEntryHorizontal
-                    icon="bp_icon.png"
-                    value="120/85"
-                    unit="mmHg"
-                    title="BP"
-                />
-            </a-col>
-        </a-row>
-
-        <a-row type="flex" justify="bottom" :gutter="[16,16]">
-            <a-col :span="8">
-                <VSEntryVertical
-                    icon="rr_icon.png"
-                    :value="60"
-                    unit="rpm"
-                    title="RR"
-                />
-            </a-col>
-            <a-col :span="8">
-                <VSEntryVertical
-                    icon="rr_icon.png"
-                    :value="60"
-                    unit="rpm"
-                    title="RR"
-                />
-            </a-col>
-            <a-col :span="8">
-                <VSEntryVertical
-                    icon="rr_icon.png"
-                    :value="60"
-                    unit="rpm"
-                    title="RR"
-                />
-            </a-col>
-        </a-row>
-        
-    </div>
-
+    <VSMultiViewChart/>
 </template>
 
 <style>
@@ -58,13 +9,11 @@
 </style>
 
 <script>
-import VSEntryHorizontal  from "../components/PatientStatusEntryCard/VSEntryHorizontal.vue";
-import VSEntryVertical  from "../components/PatientStatusEntryCard/VSEntryVertical.vue";
+import VSMultiViewChart  from "../components/ChartsPlots/VSMultiViewChart.vue";
 export default {
   name: "Home", 
   components: {
-      VSEntryVertical,
-      VSEntryHorizontal
+      VSMultiViewChart,
   },
 
   mounted(){
